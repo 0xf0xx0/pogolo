75d3ed998ba8b286ed7cb313226d16d1f14e69f6
giing <33764485+0xf0xx0@users.noreply.github.com>
2025-11-02 13:02:24 -0500

    nah no more pp compat

--- api.go
+++ api.go
@@ -10,52 +10,38 @@ import (
 // api server
 
 const API_PFX = "/api"
-const API_VER = "/v1"
+const API_VER = 1
 
-type workerInfo struct {
+type detailedWorkerInfo struct {
 	Uptime           uint64  `json:"uptime"`
-	Hashrate         float64 `json:"hashrate"` // TODO: currently mh/s, use h/s?
+	Hashrate         float64 `json:"hashrate"`
 	AcceptedShares   uint64  `json:"sharesAccepted"`
 	RejectedShares   uint64  `json:"sharesRejected"`
 	TargetDifficulty float64 `json:"targetDifficulty"`
 	BestDifficulty   float64 `json:"bestDifficulty"`
 	UserAgent        string  `json:"userAgent"`
-	Extranonce1      string  `json:"extranonce1"`
+	ExtraNonce1      string  `json:"extranonce1"`
 }
-type highScore struct {
-	UpdatedAt string  `json:"updatedAt"` // yyyy-mm-dd hh:mm:ss
-	BestDiff  float64 `json:"bestDifficulty"`
-	UserAgent string  `json:"bestDifficultyUserAgent"`
+
+// only the neccesary details
+type workerInfo struct {
+	UserAgent   string `json:"userAgent"`
+	ExtraNonce1 string `json:"extranonce1"`
 }
 type getInfoRes struct {
-	Uptime     uint64             `json:"uptime"`
-	UserAgents []getInfoUserAgent `json:"userAgents"`
-	HighScores []highScore        `json:"highScores"`
-	Tag        string             `json:"tag"`
-}
-type getInfoUserAgent struct {
-	UserAgent      string  `json:"userAgent"`
-	Uptime         uint64  `json:"uptime"`
-	BestDifficulty float64 `json:"bestDifficulty"`
-	TotalHashrate  float64 `json:"totalHashRate"`
-}
-type getPoolRes struct {
-	TotalHashrate float64 `json:"totalHashRate"`
-	TotalMiners   uint64  `json:"totalMiners"`
-	BlockHeight   uint64  `json:"blockHeight"`
-	Fee           uint64  `json:"fee"`
+	Uptime        uint64       `json:"uptime"`
+	Tag           string       `json:"tag"`
+	BlockHeight   uint64       `json:"blockHeight"`
+	TotalHashrate float64      `json:"totalHashrate"`
+	TotalWorkers  uint64       `json:"totalWorkers"`
+	Workers       []workerInfo `json:"workers"` /// TODO: come up with a cool name, swarm? gophers?
 }
 
 func initAPI() {
-	/// public-pool-ui compat endpoints
-	ppCompatPfx := "GET " + API_PFX
-	http.HandleFunc(ppCompatPfx+"/info", getInfo)
-	/// im not doin the chart
-	http.HandleFunc(ppCompatPfx+"/pool", getPool)
-	http.HandleFunc(ppCompatPfx+"/network", getNetwork)
-
-	/// ok, now our api
-	http.HandleFunc("GET "+API_PFX+API_VER+"/worker/{extranonce1}", getWorkerInfo)
+	/// TODO: wip
+	pfx := fmt.Sprintf("GET %s/v%d", API_PFX, API_VER)
+	http.HandleFunc(pfx+"/", getInfo)
+	http.HandleFunc(pfx+"/worker/{extranonce1}", getWorkerInfo)
 
 	/// default handlers
 	http.HandleFunc("GET /", func(res http.ResponseWriter, _ *http.Request) {
@@ -67,32 +53,25 @@ func initAPI() {
 }
 
 func getInfo(res http.ResponseWriter, req *http.Request) {
-	workerStats := make([]getInfoUserAgent, 0, len(clients))
-	for _, client := range clients {
-		workerStats = append(workerStats, getInfoUserAgent{
-			UserAgent:      client.Name(),
-			BestDifficulty: client.stats.bestDiff,
-			TotalHashrate:  client.stats.HashrateH(), /// public-pool-ui expects h/s
-		})
-	}
-	marshalAndWrite(res, getInfoRes{
-		Uptime:     uint64(time.Since(serverStartTime).Milliseconds()),
-		UserAgents: workerStats,
-		HighScores: getHighScores(),
-		Tag:        conf.Pogolo.Tag,
-	})
-}
-func getPool(res http.ResponseWriter, _ *http.Request) {
+	workerStats := make([]workerInfo, 0, len(clients))
 	hashrateSum := float64(0)
 	for _, client := range clients {
-		hashrateSum += client.stats.HashrateMH()
+		workerStats = append(workerStats, workerInfo{
+			UserAgent:   client.UserAgent,
+			ExtraNonce1: client.ID.String(),
+		})
+		hashrateSum += client.stats.HashrateH()
 	}
-	marshalAndWrite(res, getPoolRes{
+	marshalAndWrite(res, getInfoRes{
+		Uptime:        uint64(time.Since(serverStartTime).Milliseconds()),
+		Workers:       workerStats,
+		Tag:           conf.Pogolo.Tag,
 		TotalHashrate: hashrateSum,
-		TotalMiners:   uint64(len(clients)),
+		TotalWorkers:  uint64(len(clients)),
 		BlockHeight:   uint64(currTemplate.Height),
 	})
 }
+
 func getNetwork(res http.ResponseWriter, req *http.Request) {
 	info, err := backend.GetMiningInfo()
 	if err != nil {
@@ -111,11 +90,11 @@ func getWorkerInfo(res http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	info := workerInfo{
+	info := detailedWorkerInfo{
 		UserAgent:        client.UserAgent,
 		Uptime:           client.stats.Uptime(),
-		Extranonce1:      client.ID.String(),
-		Hashrate:         client.stats.HashrateMH(),
+		ExtraNonce1:      client.ID.String(),
+		Hashrate:         client.stats.HashrateH(),
 		TargetDifficulty: client.TargetDiff,
 		BestDifficulty:   client.stats.bestDiff,
 		AcceptedShares:   client.stats.sharesAccepted,
@@ -139,21 +118,11 @@ func marshalAndWrite(res http.ResponseWriter, v any) error {
 	}
 	res.Header().Set("Content-Type", "application/json")
 	res.Header().Set("Server", NAME+"/"+VERSION)
+	res.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
 	_, err = res.Write(x)
 	return err
 }
 
-func getHighScores() []highScore {
-	scores := make([]highScore, 0, 5)
-	for _, client := range clients {
-		scores = append(scores, highScore{
-			UpdatedAt: "", /// i dont wanna, so i wont
-			BestDiff:  client.stats.bestDiff,
-			UserAgent: client.Name(), /// lets use the name :3
-		})
-	}
-	return scores
-}
 func findClientFromName(name string) *StratumClient {
 	for _, client := range clients {
 		if client.Name() == name {
