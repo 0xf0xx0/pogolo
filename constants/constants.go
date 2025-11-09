f00916992060f57fb00b484771a0da56a1dcbbab
giing <33764485+0xf0xx0@users.noreply.github.com>
2025-11-09 17:14:40 -0500

    stratum error message cleanups

--- constants/constants.go
+++ constants/constants.go
@@ -29,21 +29,21 @@ var (
 	// server errors
 
 	// internal server error
-	ERROR_INTERNAL = stratum.Error{Code: 500, Message: "internal server error"}
+	ERROR_INTERNAL = stratum.Error{Code: 500, Message: "Internal server error"}
 	// unknown stratum method
-	ERROR_UNK_METHOD = stratum.Error{Code: 501, Message: "unknown method"}
+	ERROR_UNK_METHOD = stratum.Error{Code: 501, Message: "Unknown method"}
 	// unsupported stratum method
-	ERROR_UNSUPP_METHOD = stratum.Error{Code: 502, Message: "unsupported method"}
+	ERROR_UNSUPP_METHOD = stratum.Error{Code: 502, Message: "Unsupported method"}
 
 	// client errors
 
 	// submission before subscription
-	ERROR_NOT_SUBBED   = stratum.Error{Code: 401, Message: "not subscribed"}
-	ERROR_UNAUTHORIZED = stratum.Error{Code: 403, Message: "unauthorized"}
+	ERROR_NOT_SUBBED   = stratum.Error{Code: 401, Message: "Not subscribed"}
+	ERROR_UNAUTHORIZED = stratum.Error{Code: 403, Message: "Unauthorized"}
 	// for data we understand but will ignore, optionally disconnecting
 	ERROR_NOT_ACCEPTED = stratum.Error{Code: 406, Message: "Not accepted"}
 	ERROR_UNK_JOB      = stratum.Error{Code: 410, Message: "Unknown job"}
 	ERROR_LOW_DIFF     = stratum.Error{Code: 413, Message: "Difficulty too low"}
 	// for data we understand but couldnt process
-	ERROR_UNPROCESSABLE = stratum.Error{Code: 422, Message: "unprocessable content"}
+	ERROR_UNPROCESSABLE = stratum.Error{Code: 422, Message: "Unprocessable content"}
 )
