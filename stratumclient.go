a04752c3ab3c27de46668b9c3ff79b9052062760
giing <33764485+0xf0xx0@users.noreply.github.com>
2025-12-01 16:37:21 -0500

    adjust smoothing factor

--- stratumclient.go
+++ stratumclient.go
@@ -529,7 +529,7 @@ func (stats *ClientStats) update(currTargetDiff float64) {
 			stats.avgSubmissionDelta = delta
 		} else {
 			// avg = smoothing*delta + (1-smoothing)*avg
-			smoothing := 0.3
+			smoothing := 0.05
 			stats.avgSubmissionDelta =
 				smoothing*delta + (1-smoothing)*stats.avgSubmissionDelta
 		}
