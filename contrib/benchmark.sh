e6daba71ad274614c02c2617d9fc84fa43444685
giing <33764485+0xf0xx0@users.noreply.github.com>
2025-12-12 12:57:32 -0500

    cpuminer-opt note

--- contrib/benchmark.sh
+++ contrib/benchmark.sh
@@ -3,7 +3,7 @@ set -eu
 
 host="[::1]"
 port="5661"
-# assumes 8 thread cpu
+# assumes 8 thread cpu and cpuminer-opt
 ./cpuminer -o "$host:$port" -a sha256d --cpu-priority 5 --cpu-affinity 0x1 -q -t 1 -O "benchy1:" &
 ./cpuminer -o "$host:$port" -a sha256d --cpu-priority 5 --cpu-affinity 0x4 -q -t 1 -O "benchy2:" &
 ./cpuminer -o "$host:$port" -a sha256d --cpu-priority 5 --cpu-affinity 0x10 -q -t 1 -O "benchy3:" &
