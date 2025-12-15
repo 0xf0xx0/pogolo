d735cfbe8671a0135744160d595ed37d4fd2374d
giing <33764485+0xf0xx0@users.noreply.github.com>
2025-12-14 20:05:09 -0500

    cleanup benchmark script

--- contrib/benchmark.sh
+++ contrib/benchmark.sh
@@ -3,11 +3,12 @@ set -eu
 
 host="[::1]"
 port="5661"
+addr=""
 # assumes 8 thread cpu and cpuminer-opt
-./cpuminer -o "$host:$port" -a sha256d --cpu-priority 5 --cpu-affinity 0x1 -q -t 1 -O "benchy1:" > /dev/null &
-./cpuminer -o "$host:$port" -a sha256d --cpu-priority 5 --cpu-affinity 0x4 -q -t 1 -O "benchy2:" > /dev/null &
-./cpuminer -o "$host:$port" -a sha256d --cpu-priority 5 --cpu-affinity 0x10 -q -t 1 -O "benchy3:" > /dev/null &
-./cpuminer -o "$host:$port" -a sha256d --cpu-priority 5 --cpu-affinity 0x40 -q -t 1 -O "benchy4:" > /dev/null &
-#./cpuminer -o "$host:$port" -a sha256d --cpu-priority 5 --cpu-affinity 0x100 -q -t 1 -O "benchy5:" &
+./cpuminer -o "$host:$port" -a sha256d --cpu-priority 5 --cpu-affinity 0x1 -q -t 1 -O "$addr.benchy1:" > /dev/null &
+./cpuminer -o "$host:$port" -a sha256d --cpu-priority 5 --cpu-affinity 0x4 -q -t 1 -O "$addr.benchy2:" > /dev/null &
+./cpuminer -o "$host:$port" -a sha256d --cpu-priority 5 --cpu-affinity 0x10 -q -t 1 -O "$addr.benchy3:" > /dev/null &
+./cpuminer -o "$host:$port" -a sha256d --cpu-priority 5 --cpu-affinity 0x40 -q -t 1 -O "$addr.benchy4:" > /dev/null &
+#./cpuminer -o "$host:$port" -a sha256d --cpu-priority 5 --cpu-affinity 0x100 -q -t 1 -O "$addr.benchy5:" > /dev/null &
 
 wait
