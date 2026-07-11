f04df611bfc7b7362d07ea5d4e9bd4b2d8f18b3b
giing <33764485+0xf0xx0@users.noreply.github.com>
2026-07-11 17:07:41 -0400

    fix: actually, lets not do it backwards and try to calc the version mask from the final version for validation

--- stratumclient.go
+++ stratumclient.go
@@ -941,12 +941,10 @@ func (client *StratumClient) validateShareSubmission(share *commonShare, m *stra
 
 	/// no version rolling means the version is left untouched, its already valid
 	currJobVer := uint32(client.CurrentJob.Version)
-	masked := (share.Version & ^constants.VERSION_ROLLING_MASK)
-	if share.Version != currJobVer &&
-		// version rolling means we NAND the share version with the version mask
-		// if the result is not equal to the base block version its invalid
-		masked != 0x20000000 &&
-		masked != 0 {
+	shareMask := share.Version - currJobVer
+	masked := (shareMask & ^constants.VERSION_ROLLING_MASK)
+	// version rolling means we NAND the share version with the version mask
+	if masked != 0 {
 		println(client.CurrentJob.Version, share.Version, masked)
 		client.stats.sharesRejected++
 		if m != nil {
